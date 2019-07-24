df3e1de919e05444493bd6c6c666e4e38b62df73
fedot79 <39196719+fedot79@users.noreply.github.com>
2019-07-25 00:25:43 +0300

    25.07
    верстка готова

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Поиск по Названию </title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/e638f1d120.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 left">
+                <div class="big"><img class="big-foto" src="img/pic_tov1.jpg" alt=""></div>
+                <div class=" justify-content-around min">
+                    <img class="minImg" src="img/pic_tov1.jpg" alt="">
+                    <img class="minImg" src="img/pic_tov2.jpg" alt="">
+                    <img class="minImg" src="img/pic_tov3.jpg" alt="">
+                    <img class="minImg" src="img/pic_tov4.jpg" alt="">
+                    <img class="minImg" src="img/pic_tov5.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-md-6 justify-content-start right">
+                <h3>НАИМЕНОВАНИЕ ТОВАРА</h3>
+                <p class="article">Артикул: he002emhid73</p>
+                <p class="price">2 999 &#8381<span>3500 &#8381</span></p>
+                <p class="article-text">Здесь будет Ваш текст. Вы можете добавить краткое описание к товару, описать его свойства и уникальность</p>
+                <div class="sizes">
+                    <select class="color" name="" id="">
+                        <option value="">Цвет</option>
+                        <option value="">Белый</option>
+                        <option value="">Черный</option>
+                        <option value="">Синий</option>
+                        <option value="">Красный</option>
+                    </select>
+                    <select class="size" name="" id="">
+                        <option value="">Размер</option>
+                        <option value="">XL</option>
+                        <option value="">XS</option>
+                        <option value="">M</option>
+                        <option value="">SM</option>
+                    </select>
+                </div>
+                <div class="in-basket">
+                    <input type="number" value="1">
+                    <button type="button" class="btn btn-secondary">Добавить в корзину</button>
+                    <p><i class="fas fa-check-circle"></i><span>В наличии</span></p>
+                </div>
+                <div class="detail">
+                    <ul class="nav nav-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#!">Описание</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#!">Детали</a>
+                        </li>
+
+
+                    </ul>
+                    <p class="opis">Здесь будет Ваш текст. Вы можете добавить краткое описание к товару, описать его свойства и уникальность.
+                        Здесь будет Ваш текст. Вы можете добавить краткое описание к товару, описать его свойства и уникальность
+                        Здесь будет Ваш текст. Вы можете добавить краткое описание к товару, описать его свойства и уникальность.
+
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <p class="metki">Категории: &nbsp; Женская, Футболки <br>
+                    Метки: &nbsp; Ralflauren, Поло, Футболка</p>
+                </div>
+                <p class="seti">Поделиться</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
